eaddb1f8afc5e8802cd4b7ea4a1214f16cd592c1
krishnamoorthy2000 <krishnamalathi2000@gmail.com>
2023-04-29 05:04:24 +0530

    second commit

--- routes/forgotpassword.js
+++ routes/forgotpassword.js
@@ -2,39 +2,33 @@ const express = require("express");
 const router = express.Router();
 const nodemailer = require("nodemailer");
 const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
 const userSchema = require("../schema/schema");
+const crypto = require("crypto");
 
 require("dotenv").config();
 
-const secretKey = process.env.JWT_SECRET_KEY;
-const tokenExpiration = process.env.JWT_TOKEN_EXPIRATION;
+// const secretKey = process.env.JWT_SECRET_KEY;
+// const tokenExpiration = process.env.JWT_TOKEN_EXPIRATION;
 
 // Generate a JSON Web Token (JWT) for password reset
-const generateResetToken = (email) => {
-  const token = jwt.sign({ email }, secretKey, { expiresIn: tokenExpiration });
-  return token;
-};
+// const generateResetToken = (email) => {
+//   const token = jwt.sign({ email }, secretKey, { expiresIn: tokenExpiration });
+//   return token;
+// };
 
 // Send a password reset email with the reset token
-const sendResetEmail = async (email, token, protocol, host) => {
+const sendResetEmail = async (email, token) => {
   const transporter = nodemailer.createTransport({
-    // host: process.env.SMTP_HOST,
-    // port: process.env.SMTP_PORT,
-    // secure: true,
-    service:"gmail",
+    service: "gmail",
     auth: {
-      user: process.env.SMTP_USER,
-      pass: process.env.SMTP_PASSWORD,
-    }
-    
+      user: process.env.USER,
+      pass: process.env.PASSWORD,
+    },
   });
-  
-  
 
   const resetLink = `http://localhost:3000/forgotpassword/resetpassword/${token}`;
   const mailOptions = {
-    from: process.env.SMTP_FROM,
+    from: process.env.FROM,
     to: email,
     subject: "Password Reset",
     html: `Click <a href="${resetLink}">here</a> to reset your password`,
@@ -57,14 +51,14 @@ router.post("/", async (req, res) => {
     }
 
     // Generate a reset token and save it to the user's document
-    const resetToken = generateResetToken(email);
+    const resetToken = crypto.randomBytes(20).toString("hex");
+
     user.resetToken = resetToken;
     await user.save();
 
     // Send a password reset email with the reset token
-    const protocol = req.protocol;
-    const host = req.get("host");
-    await sendResetEmail(email, resetToken, protocol, host);
+    
+    await sendResetEmail(email, resetToken);
 
     res.status(200).json({ message: "Password reset email sent" });
   } catch (err) {
@@ -89,12 +83,12 @@ router.post("/resetpassword/:token", async (req, res) => {
     }
 
     // Check if reset token has expired
-    const tokenExpirationDate = jwt.decode(token).exp;
-    const now = new Date().getTime() / 1000;
-    if (tokenExpirationDate < now) {
-      console.log("Reset token has expired");
-      return res.status(400).json({ message: "Reset token has expired" });
-    }
+    // const tokenExpirationDate = jwt.decode(token).exp;
+    // const now = Math.floor(Date.now() / 1000); // convert current time to seconds
+    // if (tokenExpirationDate < now) {
+    //   console.log("Reset token has expired");
+    //   return res.status(400).json({ message: "Reset token has expired" });
+    // }
 
     // Hash the new password
     const hashedPassword = await bcrypt.hash(password, 10);
@@ -105,10 +99,12 @@ router.post("/resetpassword/:token", async (req, res) => {
     await user.save();
 
     console.log("Password has been updated successfully");
-    return res.status(200).json({ message: "Password has been changed successfully" });
+    return res
+      .status(200)
+      .json({ message: "Password has been changed successfully" });
   } catch (err) {
     console.log("Error updating password:", err);
     return res.status(500).json({ message: "Error updating password" });
   }
 });
-module.exports=router;
+module.exports = router;
